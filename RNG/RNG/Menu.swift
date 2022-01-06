756b46f649652928d1b03854dd020c658c2dbbc3
speedmaster24 <speedmaster2424@gmail.com>
2022-01-06 08:31:31 -0700

    rng project

---
+++ RNG/RNG/Menu.swift
@@ -0,0 +1,21 @@
+//
+//  Menu.swift
+//  RNG
+//
+//  Created by speed Master on 1/4/22.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text("hello")
+        
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
---
+++ RNG/RNG/RNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  RNGApp.swift
+//  RNG
+//
+//  Created by speed Master on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
