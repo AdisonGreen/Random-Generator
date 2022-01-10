dfbb7f8a592e8337c53005537109e3eb00570174
Matthew <matt@Matthews-MacBook-Pro.local>
2022-01-10 09:42:27 -0700

    Card Picker Done
    Finished the Basic Card Picker. Just the card picker no Options

--- RNG/RNG/Card Picker.swift
+++ RNG/RNG/Card Picker.swift
@@ -22,110 +22,119 @@ struct Card_Picker: View {
                 Button(action: {
                     let randomInt = Int.random(in: 1...52)
                     print(randomInt)
+                    //MARK: Hearts
                     if randomInt == 1 {
                         startingImage = Image("ace_of_hearts")
                     } else if randomInt == 2 {
-                        startingImage = Image("ace_of_diamonds")
+                        startingImage = Image("2_of_hearts")
                     } else if randomInt == 3 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("3_of_hearts")
                     } else if randomInt == 4 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("4_of_hearts")
                     } else if randomInt == 5 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("5_of_hearts")
                     } else if randomInt == 6 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("6_of_hearts")
                     } else if randomInt == 7 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("7_of_hearts")
                     } else if randomInt == 8 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("8_of_hearts")
                     } else if randomInt == 9 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("9_of_hearts")
                     } else if randomInt == 10 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("10_of_hearts")
                     } else if randomInt == 11 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("jack_of_hearts2")
                     } else if randomInt == 12 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("queen_of_hearts2")
                     } else if randomInt == 13 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("king_of_hearts2")
+                    //MARK: Diamonds
                     } else if randomInt == 14 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("ace_of_diamonds")
                     } else if randomInt == 15 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("2_of_diamonds")
                     } else if randomInt == 16 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("3_of_diamonds")
                     } else if randomInt == 17 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("4_of_diamonds")
                     } else if randomInt == 18 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("5_of_diamonds")
                     } else if randomInt == 19 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("6_of_diamonds")
                     } else if randomInt == 20 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("7_of_diamonds")
                     } else if randomInt == 21 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("8_of_diamonds")
                     } else if randomInt == 22 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("9_of_diamonds")
                     } else if randomInt == 23 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("10_of_diamonds")
                     } else if randomInt == 24 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("jack_of_diamonds2")
                     } else if randomInt == 25 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("queen_of_diamonds2")
                     } else if randomInt == 26 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("king_of_diamonds2")
                     } else if randomInt == 27 {
+                    //MARK: Spades
                         startingImage = Image("ace_of_spades2")
                     } else if randomInt == 28 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("2_of_spades")
                     } else if randomInt == 29 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("3_of_spades")
                     } else if randomInt == 30 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("4_of_spades")
                     } else if randomInt == 31 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("5_of_spades")
                     } else if randomInt == 32 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("6_of_spades")
                     } else if randomInt == 33 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("7_of_spades")
                     } else if randomInt == 34 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("8_of_spades")
                     } else if randomInt == 35 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("9_of_spades")
                     } else if randomInt == 36 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("10_of_spades")
                     } else if randomInt == 37 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("jack_of_spades2")
                     } else if randomInt == 38 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("queen_of_spades2")
                     } else if randomInt == 39 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("king_of_spades2")
                     } else if randomInt == 40 {
-                        startingImage = Image("ace_of_spades2")
+                    //MARK: Clubs
+                        startingImage = Image("ace_of_clubs")
                     } else if randomInt == 41 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("2_of_clubs")
                     } else if randomInt == 42 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("3_of_clubs")
                     } else if randomInt == 43 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("4_of_clubs")
                     } else if randomInt == 44 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("5_of_clubs")
                     } else if randomInt == 45 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("6_of_clubs")
                     } else if randomInt == 46 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("7_of_clubs")
                     } else if randomInt == 47 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("8_of_clubs")
                     } else if randomInt == 48 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("9_of_clubs")
                     } else if randomInt == 49 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("10_of_clubs")
                     } else if randomInt == 50 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("jack_of_clubs2")
                     } else if randomInt == 51 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("queen_of_clubs2")
                     } else if randomInt == 52 {
-                        startingImage = Image("ace_of_spades2")
+                        startingImage = Image("king_of_clubs2")
+                    //MARK: Jokers
+                    } else if randomInt == 53 {
+                        startingImage = Image("black_joker")
+                    } else if randomInt == 54 {
+                        startingImage = Image("red_joker")
                     }
                 }) {
                     Text("Pick A Card")
