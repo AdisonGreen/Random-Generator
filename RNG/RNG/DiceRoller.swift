ce872d5786c3df104f7f4c8bbb3875429b7a37fb
Matthew <matt@Matthews-MacBook-Pro.local>
2022-01-10 08:55:03 -0700

    Dice Roller
    Basic Dice Roller finished

--- RNG/RNG/DiceRoller.swift
+++ RNG/RNG/DiceRoller.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct DiceRoller: View {
+    @State private var startingImage = Image("Dice 2")
+    //need to make this so that it starts with a random dice not just dice 2
+    
     var body: some View {
         NavigationView {
             VStack {
-                Image("Dice 4")
+                startingImage
                     .resizable()
                     .scaledToFit()
                     .position(x: 200, y: 200)
-                    
 
                 Button(action: {
-                    print("Button Pressed")
+                    let randomInt = Int.random(in: 1...6)
+                    if randomInt == 1 {
+                        startingImage = Image("Dice 1")
+                    } else if randomInt == 2 {
+                        startingImage = Image("Dice 2")
+                    } else if randomInt == 3 {
+                        startingImage = Image("Dice 3")
+                    } else if randomInt == 4 {
+                        startingImage = Image("Dice 4")
+                    } else if randomInt == 5 {
+                        startingImage = Image("Dice 5")
+                    } else if randomInt == 6 {
+                        startingImage = Image("Dice 6")
+                    }
                 }) {
                     Text("Roll Dice")
                         .font(.title3)
