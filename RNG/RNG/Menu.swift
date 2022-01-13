92f13d0ee6c2f514fa76ded0f1d57df65f35e758
speedmaster24 <speedmaster2424@gmail.com>
2022-01-13 08:09:02 -0700

    did the tab bar for main hud
    added an image for the wheel icon

--- RNG/RNG/Menu.swift
+++ RNG/RNG/Menu.swift
@@ -9,121 +9,139 @@ import SwiftUI
 
 struct Menu: View {
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    HStack {
-                        // RANDOM NUMBER GENERATOR BUTTON
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("HashtagRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
+        TabView {
+            NavigationView {
+                ScrollView {
+                    VStack {
+                        HStack {
+                            // RANDOM NUMBER GENERATOR BUTTON
+                            Button {
+                                print("random number generator")
+                            } label: {
+                                Image("HashtagRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            // DICE ROLLER GENERATOR
+                            Button {
+                                print("dice roller")
+                            } label: {
+                                Image("DiceRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            // RANDOM CARD PICKER GENERTOR
+                            Button {
+                                print("card picker")
+                            } label: {
+                                Image("CardRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
                         }
-                        // DICE ROLLER GENERATOR
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("DiceRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
+                        HStack {
+                            Button {
+                                print("Wheel spinner")
+                            } label: {
+                                Image("WheelRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
                         }
-                        // RANDOM CARD PICKER GENERTOR
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("CardRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
+                        HStack {
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                        }
+                        HStack {
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Button {
+                                print("yessss")
+                            } label: {
+                                Image("BlankRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
                         }
                     }
-                    HStack {
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                    }
-                    HStack {
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                    }
-                    HStack {
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                        Button {
-                            print("yessss")
-                        } label: {
-                            Image("BlankRounded")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding(5)
-                        }
-                    }
+                    //scrollview
                 }
-                //scrollview
+                .navigationTitle("Random generators")
+                //navView
             }
-            .navigationTitle("Random generators")
-            //nav view
+            .tabItem {
+                Image(systemName: "infinity.circle.fill")
+                Text("Generators")
+            }
+            //so long as this text is commented out this tab bar item wont show
+            //Text("fancy generators")
+            .tabItem {
+                Image(systemName: "gear.circle.fill")
+                Text("Fancy Generators")
+            }
+            Text("Lists")
+            .tabItem {
+                Image(systemName: "list.bullet")
+                Text("Lists")
+            }
+            //tabView
         }
     }
 }
