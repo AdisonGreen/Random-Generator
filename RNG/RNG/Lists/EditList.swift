181e2ba7250f37071367897264218014848f6b09
speedmaster2424 <89266913+speedmaster2424@users.noreply.github.com>
2022-01-20 09:15:59 -0700

    STUFF (#24)
    * Bunch of fixes
    
    *FIxed the magic 8 ball image constraints i believe
    *swaped out the 8ball menu image since i forgot
    *put in an image of an old film projector for what would later be the random advert menu (made in freetime)
    *quickly added labels to all the buttons
    
    * stuff

---
+++ RNG/RNG/Lists/EditList.swift
@@ -0,0 +1,61 @@
+//
+//  ListTabBar.swift
+//  RNG
+//
+//  Created by Adison Green on 1/10/22.
+//
+
+import SwiftUI
+
+struct EditList: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var listChosen: UserList
+    @StateObject var userLists: UserLists
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("List Name")) {
+                    TextField("Name of your list", text: $listChosen.listName)
+                }
+                
+                Section(header: Text("List Items")) {
+                    ForEach(0..<listChosen.listItems.count, id: \.self) { item in
+                        TextField("One item from you list", text: $listChosen.listItems[item])
+                        
+                    }
+                }
+                
+                Button {
+                    listChosen.listItems.append("")
+                } label: {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "plus")
+                        Spacer()
+                    }
+                }
+            }
+            .navigationTitle("Edit Lists")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        if listChosen.newList {
+                            userLists.lists.append(listChosen)
+                            listChosen.newList = false
+                        }
+                        
+                        userLists.objectWillChange.send()
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+//struct EditList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EditList(listChosen: UserList(listName: "Some dude", listItems: ["What", "is", "this", "for"]))
+//    }
+//}
