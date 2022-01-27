daf1985adb71f1b65ae85966b3bfdbd84a0c5657
Adison Green <adison@Adisons-MacBook-Pro.local>
2022-01-27 08:46:54 -0700

    Almost fixed bugs

--- RNG/RNG/Menu.swift
+++ RNG/RNG/Menu.swift
@@ -12,163 +12,145 @@ struct Menu: View {
     
     var body: some View {
         NavigationView {
-            TabView {
-                ScrollView {
-                    VStack {
-                        HStack {
-                            VStack {
-                                // RANDOM NUMBER GENERATOR BUTTON
-                                NavigationLink(destination: Random_Number_Gen()) {
-                                    Image("HashtagRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Random Number Generator")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
+            ScrollView {
+                VStack {
+                    HStack {
+                        VStack {
+                            // RANDOM NUMBER GENERATOR BUTTON
+                            NavigationLink(destination: Random_Number_Gen()) {
+                                Image("HashtagRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
                             }
-                            VStack {
-                                // DICE ROLLER GENERATOR
-                                NavigationLink(destination: DiceRoller()) {
-                                    Image("DiceRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Dice Roller \n")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
-                            }
-                            VStack {
-                                // RANDOM CARD PICKER GENERTOR
-                                NavigationLink(destination: Card_Picker()) {
-                                    Image("CardRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Card Picker \n")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
-                            }
+                            Text("Random Number Generator")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
                         }
-                        HStack {
-                            VStack {
-                                //Spinner
-                                NavigationLink(destination: Random_Number_Gen()) {
-                                    Image("WheelRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Spinner")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
+                        VStack {
+                            // DICE ROLLER GENERATOR
+                            NavigationLink(destination: DiceRoller()) {
+                                Image("DiceRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
                             }
-                            VStack {
-                                //Magic 8Ball
-                                NavigationLink(destination: EightBall()) {
-                                    Image("8BallRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Magic EightBall")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
-                            }
-                            VStack {
-                                //Magic 8Ball
-                                NavigationLink(destination: Random_Number_Gen()) {
-                                    Image("ProjectorRounded")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding(5)
-                                }
-                                Text("Advertisement")
-                                    .font(.subheadline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.center)
-                            }
+                            Text("Dice Roller \n")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
                         }
-                        HStack {
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
+                        VStack {
+                            // RANDOM CARD PICKER GENERTOR
+                            NavigationLink(destination: Card_Picker()) {
+                                Image("CardRounded")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .padding(5)
                             }
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(5)
-                            }
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(5)
-                            }
-                        }
-                        HStack {
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(5)
-                            }
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(5)
-                            }
-                            Button {
-                                print("yessss")
-                            } label: {
-                                Image("BlankRounded")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .padding(5)
-                            }
+                            Text("Card Picker \n")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
                         }
                     }
-                } //scrollView Closing bracket
-                .tabItem {
-                    Image(systemName: "infinity.circle.fill")
-                    Text("Generators")
-                }
-                //so long as this text is commented out this tab bar item wont show
-                //Text("fancy generators")
-                .tabItem {
-                    Image(systemName: "gear.circle.fill")
-                    Text("Fancy Generators")
-                }
-                Lists()
-                    .tabItem {
-                        Image(systemName: "list.bullet")
-                        Text("Lists")
+                    HStack {
+                        VStack {
+                            //Spinner
+                            NavigationLink(destination: Random_Number_Gen()) {
+                                Image("WheelRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Text("Spinner")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                        }
+                        VStack {
+                            //Magic 8Ball
+                            NavigationLink(destination: EightBall()) {
+                                Image("8BallRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Text("Magic EightBall")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                        }
+                        VStack {
+                            //Magic 8Ball
+                            NavigationLink(destination: Random_Number_Gen()) {
+                                Image("ProjectorRounded")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(5)
+                            }
+                            Text("Advertisement")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                        }
                     }
-                    .navigationTitle("Random Generators")
-            } //tabView closing bracket
-            .navigationTitle("Random Generators")
+                    HStack {
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                    }
+                    HStack {
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                        Button {
+                            print("yessss")
+                        } label: {
+                            Image("BlankRounded")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(5)
+                        }
+                    }
+                }
+            } //scrollView Closing bracket
+                .navigationTitle("Random Generators")
         } //nav view closing bracket
     }
 }
