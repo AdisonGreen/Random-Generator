81b054a1fca6964e29edbf4efb68c43d7670fd4d
speedmaster2424 <89266913+speedmaster2424@users.noreply.github.com>
2022-01-27 08:40:53 -0700

    added rng file (#41)

---
+++ RNG/RNG/RNG programs/Ad Generator.swift
@@ -0,0 +1,20 @@
+//
+//  Ad Generator.swift
+//  RNG
+//
+//  Created by speed Master on 1/26/22.
+//
+
+import SwiftUI
+
+struct Ad_Generator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Ad_Generator_Previews: PreviewProvider {
+    static var previews: some View {
+        Ad_Generator()
+    }
+}
