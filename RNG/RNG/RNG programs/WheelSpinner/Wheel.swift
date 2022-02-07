e1bc8777fe0ee04671ce10f71990b67b9f3b770b
Adison Green <adison@adisons-mbp.lan>
2022-02-07 09:07:30 -0700

    Wheel is getting closeish

--- RNG/RNG/RNG programs/WheelSpinner/Wheel.swift
+++ RNG/RNG/RNG programs/WheelSpinner/Wheel.swift
@@ -34,134 +34,25 @@ struct drawWheel: InsettableShape {
 }
 
 struct Wheel: View {
-    let myChosenListItems = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    let myChosenListItems = ["a", "b", "c", "d"]
     
     var body: some View {
         ZStack {
-            ForEach(0..<2) { item in
-                if myChosenListItems.count == 1 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.green)
-                } else if myChosenListItems.count == 2 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(180), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(180), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.blue)
-                } else if myChosenListItems.count == 3 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(120), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(120), endAngle: .degrees(240), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(240), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.yellow)
-                } else if myChosenListItems.count == 4 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(90), endAngle: .degrees(180), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(180), endAngle: .degrees(270), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(270), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.red)
-                } else if myChosenListItems.count == 5 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(72), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(72), endAngle: .degrees(144), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(144), endAngle: .degrees(216), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(216), endAngle: .degrees(288), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(288), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.red)
-                } else if myChosenListItems.count == 6 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(60), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(60), endAngle: .degrees(120), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(120), endAngle: .degrees(180), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(180), endAngle: .degrees(240), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(240), endAngle: .degrees(300), clockwise: true)
-                        .foregroundColor(.red)
-                    drawWheel(startAngle: .degrees(300), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.yellow)
-                } else if myChosenListItems.count == 7 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(51.42), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(51.42), endAngle: .degrees(102.84), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(102.84), endAngle: .degrees(154.26), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(154.26), endAngle: .degrees(205.68), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(205.68), endAngle: .degrees(257.1), clockwise: true)
-                        .foregroundColor(.red)
-                    drawWheel(startAngle: .degrees(257.1), endAngle: .degrees(308.52), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(308.52), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.green)
-                } else if myChosenListItems.count == 8 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(45), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(45), endAngle: .degrees(90), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(90), endAngle: .degrees(135), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(135), endAngle: .degrees(180), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(180), endAngle: .degrees(225), clockwise: true)
-                        .foregroundColor(.red)
-                    drawWheel(startAngle: .degrees(225), endAngle: .degrees(270), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(270), endAngle: .degrees(315), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(315), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.red)
-                } else if myChosenListItems.count == 9 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(40), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(40), endAngle: .degrees(80), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(80), endAngle: .degrees(120), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(120), endAngle: .degrees(160), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(160), endAngle: .degrees(200), clockwise: true)
-                        .foregroundColor(.red)
-                    drawWheel(startAngle: .degrees(200), endAngle: .degrees(240), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(240), endAngle: .degrees(280), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(280), endAngle: .degrees(320), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(320), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.green)
-                } else if myChosenListItems.count == 10 {
-                    drawWheel(startAngle: .degrees(0), endAngle: .degrees(36), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(36), endAngle: .degrees(72), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(72), endAngle: .degrees(108), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(108), endAngle: .degrees(144), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(144), endAngle: .degrees(180), clockwise: true)
-                        .foregroundColor(.red)
-                    drawWheel(startAngle: .degrees(180), endAngle: .degrees(216), clockwise: true)
-                        .foregroundColor(.blue)
-                    drawWheel(startAngle: .degrees(216), endAngle: .degrees(252), clockwise: true)
-                        .foregroundColor(.orange)
-                    drawWheel(startAngle: .degrees(252), endAngle: .degrees(288), clockwise: true)
-                        .foregroundColor(.yellow)
-                    drawWheel(startAngle: .degrees(288), endAngle: .degrees(324), clockwise: true)
-                        .foregroundColor(.green)
-                    drawWheel(startAngle: .degrees(324), endAngle: .degrees(360), clockwise: true)
-                        .foregroundColor(.red)
-                }
+            ForEach(myChosenListItems.indices, id: \.self) { item in
+                let sizeOfWheelSlice = 360 / myChosenListItems.count
+                
+                let sizeOfPie = Double(sizeOfWheelSlice)
+                let startPoint = Double(sizeOfWheelSlice) * Double(item + 1)
+                let endPoint = sizeOfPie + startPoint
+                
+                drawWheel(startAngle: .degrees(startPoint), endAngle: .degrees(endPoint), clockwise: true)
+                    .foregroundColor(item % 2 == 0 ? .red : .blue)
             }
         }
+    }
+    
+    func chooseAColor() {
+        
     }
 }
 
