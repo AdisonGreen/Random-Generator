18151684087f37b4aa1c25e1e78f459be453eb54
Matthew <matt@Matthews-MacBook-Pro.local>
2022-02-08 09:28:55 -0700

    Forgot to Add
    Forgot To Add to last commit

--- RNG/RNG/RNG programs/DiceRoller/DiceView.swift
+++ RNG/RNG/RNG programs/DiceRoller/DiceView.swift
@@ -4,17 +4,198 @@
 //
 //  Created by Matthew on 2/8/22.
 //
+//NEED TO MAKE IT USE THIS INSTEAD OF USING "DICEROLLER"
 
 import SwiftUI
 
 struct DiceView: View {
+    var startingImage = Image("DiceSideOne")
+    var diceVisible = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if diceVisible == 1 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 200, maximum: 200))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 2 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 160))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 3 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 4 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 5 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 6 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 7 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 8 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            if diceVisible == 9 {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 100))]) {
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                    startingImage
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            
+            startingImage
+                .resizable()
+                .scaledToFit()
+                .padding()
+                .opacity(0.0)
+        }
     }
-}
-
-struct DiceView_Previews: PreviewProvider {
-    static var previews: some View {
-        DiceView()
+    
+    struct DiceView_Previews: PreviewProvider {
+        static var previews: some View {
+            DiceView()
+        }
     }
 }
