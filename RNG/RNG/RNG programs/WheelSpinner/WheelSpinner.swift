63c2793e9729a9444cc7249b15fca5cf333c45ff
Adison Green <adison@Adisons-MacBook-Pro.local>
2022-02-11 09:32:01 -0700

    Feels close but I want to try something else

--- RNG/RNG/RNG programs/WheelSpinner/WheelSpinner.swift
+++ RNG/RNG/RNG programs/WheelSpinner/WheelSpinner.swift
@@ -9,38 +9,49 @@ import SwiftUI
 
 struct WheelSpinner: View {
     @State private var userSelection = ListsController.shared.userLists.lists.first ?? UserList(listName: "", listItems: ["You have no list selected"], newList: false)
-    @State var randomItem = ""
+    @State private var randomItem = ""
+    @State private var wheelStopped = 0.0
     
     @State private var animationAmount = 0.0
+    @State private var staringAnimation = 0.0
     
     @State private var isAnimating = false
     
     var body: some View {
+        let myWheel = Wheel(myChosenListItems: userSelection.listItems)
         VStack {
             Spacer()
             Spacer()
             ListPicker(selection: $userSelection)
+            Text(randomItem)
             Spacer()
             VStack(spacing: -70) {
                 WheelArrow()
                     .zIndex(1)
-                Wheel(myChosenListItems: userSelection.listItems)
+                
+                myWheel
+                    .rotationEffect(.degrees(wheelStopped))
                     .rotationEffect(.degrees(animationAmount))
                     .animation(.easeOut(duration: 5), value: animationAmount)
             }
             Spacer()
             Button("Spin") {
+                
                 let sliceDegrees = Double(360 / userSelection.listItems.count)
                 
                 let randomDegree = Int.random(in: 0...360)
                 let randomDegreeInDouble = Double(randomDegree)
                 
+                
                 let whereWheelWillLand = randomDegreeInDouble / sliceDegrees
+                let whereWheelWillLandInInt = Int(whereWheelWillLand - 1)
+                randomItem = userSelection.listItems[whereWheelWillLandInInt]
                 
                 let randomSpins = [2160.0, 2520.0, 2880.0, 3240.0, 3600.0]
                 let howManySpins = randomDegreeInDouble + randomSpins.randomElement()!
-                animationAmount += howManySpins
-                randomItem = ""
+                animationAmount += howManySpins - (howManySpins * 2)
+                wheelStopped = randomDegreeInDouble - (randomDegreeInDouble * 2)
+//                isAnimating = true
             }
             .alert("\(randomItem)", isPresented: $isAnimating) {
                 Button("Ok", role: .cancel) { }
