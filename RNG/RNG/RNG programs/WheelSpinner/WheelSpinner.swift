a0bc0eebaba56f40fbe35dbe1931d8e8b07942fa
Adison Green <adison@Adisons-MacBook-Pro.local>
2022-02-11 10:04:02 -0700

    VERY CLOSE

--- RNG/RNG/RNG programs/WheelSpinner/WheelSpinner.swift
+++ RNG/RNG/RNG programs/WheelSpinner/WheelSpinner.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct WheelSpinner: View {
     @State private var userSelection = ListsController.shared.userLists.lists.first ?? UserList(listName: "", listItems: ["You have no list selected"], newList: false)
     @State private var randomItem = ""
-    @State private var wheelStopped = 0.0
     
     @State private var animationAmount = 0.0
-    @State private var staringAnimation = 0.0
+    
+    @State private var resetAnimationAmount = 0.0
+    @State private var resetWheel = 0.0
     
     @State private var isAnimating = false
     
     var body: some View {
-        let myWheel = Wheel(myChosenListItems: userSelection.listItems)
         VStack {
             Spacer()
             Spacer()
@@ -28,29 +28,33 @@ struct WheelSpinner: View {
             VStack(spacing: -70) {
                 WheelArrow()
                     .zIndex(1)
-                
-                myWheel
-                    .rotationEffect(.degrees(wheelStopped))
+                Wheel(myChosenListItems: userSelection.listItems)
                     .rotationEffect(.degrees(animationAmount))
                     .animation(.easeOut(duration: 5), value: animationAmount)
+                    .rotationEffect(.degrees(resetAnimationAmount))
             }
             Spacer()
             Button("Spin") {
-                
                 let sliceDegrees = Double(360 / userSelection.listItems.count)
+                let halfSliceDegrees = sliceDegrees / 2.0
                 
-                let randomDegree = Int.random(in: 0...360)
-                let randomDegreeInDouble = Double(randomDegree)
+                resetAnimationAmount += (resetWheel + halfSliceDegrees)
                 
+                let randomIndex = Int.random(in: 0..<userSelection.listItems.count)
                 
-                let whereWheelWillLand = randomDegreeInDouble / sliceDegrees
-                let whereWheelWillLandInInt = Int(whereWheelWillLand - 1)
-                randomItem = userSelection.listItems[whereWheelWillLandInInt]
+                let degreesOfRotation = Double(randomIndex + 1) * sliceDegrees
+                
+//                let randomDegree = Int.random(in: 0...360)
+//                let randomDegreeInDouble = Double(randomDegree)
+                
+//                let whereWheelWillLand = randomDegreeInDouble / sliceDegrees
+//                let whereWheelWillLandInInt = Int(whereWheelWillLand - 1)
+                randomItem = userSelection.listItems[randomIndex]
                 
                 let randomSpins = [2160.0, 2520.0, 2880.0, 3240.0, 3600.0]
-                let howManySpins = randomDegreeInDouble + randomSpins.randomElement()!
-                animationAmount += howManySpins - (howManySpins * 2)
-                wheelStopped = randomDegreeInDouble - (randomDegreeInDouble * 2)
+                let howManySpins = degreesOfRotation + randomSpins.randomElement()!
+                resetWheel = howManySpins
+                animationAmount += (howManySpins - (howManySpins * 2)) + (halfSliceDegrees - (halfSliceDegrees * 2))
 //                isAnimating = true
             }
             .alert("\(randomItem)", isPresented: $isAnimating) {
