d63e9d16f52c5ffd05eb005e6c1ff00347adc58d
Matthew <matt@Matthews-MacBook-Pro.local>
2022-02-16 08:57:37 -0700

    forgot to add
    forgot to add a this to the last commit

--- RNG/RNG/MainPageSettings.swift
+++ RNG/RNG/MainPageSettings.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct MainPageSettings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Project Created by:")
+                .padding(EdgeInsets(top: -320, leading: -135, bottom: 0, trailing: 0))
+                .font(Font.title.weight(.semibold))
+            Text("Max Josie")
+                .padding(EdgeInsets(top: -290, leading: -179, bottom: 0, trailing: 0))
+                .font(Font.title3.weight(.light))
+            Text("Adison Green")
+                .padding(EdgeInsets(top: -275, leading: -179, bottom: 0, trailing: 0))
+                .font(Font.title3.weight(.light))
+            Text("Matthew Altamiranda")
+                .padding(EdgeInsets(top: -260, leading: -175, bottom: 0, trailing: 0))
+                .font(Font.title3.weight(.light))
+            
+        }
+
     }
 }
 
