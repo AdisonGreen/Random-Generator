348509d4719c1cc7bca5ee0b46ca416d454143b7
speedmaster24 <speedmaster2424@gmail.com>
2022-02-17 10:28:49 -0700

    final changes

--- RNG/RNG/Menu/Settings Menu/Credits.swift
+++ RNG/RNG/Menu/Settings Menu/Credits.swift
@@ -19,9 +19,9 @@ struct Credits: View {
                 .fontWeight(.medium)
             Text("""
 
-adison green
+Adison Green
 Speedmaster24
-matthew altaniranda
+Matthew Altaniranda
 
 """)
                 .multilineTextAlignment(.center)
