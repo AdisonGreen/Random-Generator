9c08239e47e208090818895b076f2eda2e8a576b
Matthew <matt@Matthews-MacBook-Pro.local>
2022-02-17 22:56:20 -0700

    credits look cleaner now

--- RNG/RNG/Menu/Settings Menu/Credits.swift
+++ RNG/RNG/Menu/Settings Menu/Credits.swift
@@ -18,40 +18,36 @@ struct Credits: View {
                 .font(.title3)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
-            Text("""
-
-Adison Green
-Speedmaster24
-Matthew Altaniranda
-
-""")
-                .multilineTextAlignment(.center)
+            VStack {
+                Text("- Adison Green")
+                    .padding(1)
+                Text("- Speedmaster24")
+                    .padding(1)
+                Text("- Matthew Altamiranda")
+                    .padding(1)
+            }
+            .padding(2)
+            .multilineTextAlignment(.center)
             Text("Additional help")
                 .font(.title3)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
-            Text("""
-
-Alan Barth
-SwiftyPlant
-
-""")
-                .multilineTextAlignment(.center)
+            VStack {
+                Text("- Alan Barth")
+                    .padding(1)
+                Text("- SwiftyPlant")
+                    .padding(1)
+            }
             Text("Side note")
                 .font(.title3)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
-            Text("""
-
-This App was a project made for a college class.
-It is the culmination of 6 weeks of work however we the aformentioned app devs do plan to keep working on it even afterwards and turn this into a great app that wont ever have in app purchases or annoying ads that appear every few seconds however to do that we could use your support so feel free to donate to our patreon
-if you have any ideas message speedmaster24 at his email of
-speedmaster2425@gmail.com
-""")
-            
+                .padding(2)
+            Text("This App was a project made for a college class. It is the progress of 6 weeks. The App developers have decided that we will keep progressing on the app afterwards and make it so that it does not have any type of in app purchases or ads. However if you would like to support us feel free to donate to our patreon. Any further questions or ideas email us at speedmaster2425@gmail.com")
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 5.0)
         }
+        .padding(.top, -50)
     }
 }
 
